73bd0b85117f4ec19ad98bbc1b5b57f9d63278fc
michaelfm1211 <42911957+michaelfm1211@users.noreply.github.com>
2020-03-15 19:00:04 -0500

    Added 'User has joined the chat' messages on login

--- login.php
+++ login.php
@@ -11,8 +11,11 @@ $client->addScope('https://www.googleapis.com/auth/userinfo.profile');
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
 	$client->setAccessToken($_SESSION['access_token']);
 	$oauth2 = new \Google_Service_Oauth2($client);
-	$_SESSION['user_info'] = $oauth2->userinfo->get();
-	$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/';
+	$user_info = $oauth2->userinfo->get();
+	$_SESSION['user_info'] = $user_info;
+
+	$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/chat_message.php?msg=' . $user_info['given_name'] . "+" . $user_info['family_name'][0] . "+had+joined+the+chat.&redirect_uri=http%3A%2F%2Flocalhost%2F";	// Chat that the user has joined the chat, then redirect to /
+	
 	header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 } else {
  	$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
